9c6ae0280d80b61bce792d8a84b4354fedd5bc07
dell <dell@DESKTOP-UVPQMLU>
2023-10-04 16:31:47 +0530

    commit

---
+++ FibonacciSeries.java
@@ -0,0 +1,18 @@
+
+public class FibonacciSeries {
+
+	public static void main(String[] args) {
+		int a=0,b=1;
+		System.out.print(a+" "+b);
+		int c;
+		for (int i=1;i<=12;i++)
+		{
+			c=a+b;
+			System.out.print(" "+c);
+			a=b;
+			b=c;
+		}
+
+	}
+
+}
